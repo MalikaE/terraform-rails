e7ec5f0c271416f5c5d04242d7e085358f15cd0c
MalikaE <99386572+MalikaE@users.noreply.github.com>
2023-08-03 13:55:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,3 @@ resource "aws_instance" "foo" {
   ami           = "ami-053b0d53c279acc90" 
   instance_type = "t2.micro"
 }
-  network_interface {
-    network_interface_id = aws_network_interface.foo.id
-    device_index         = 0
-  }
